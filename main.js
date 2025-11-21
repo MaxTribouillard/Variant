e896a45ee81cbe1ed102ca729482e67fae1ae546
Max <57811248+MaxTribouillard@users.noreply.github.com>
2025-11-21 16:59:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+var engine,
+  scene,
+  engineMat,
+  mats,
+  canvas = null;
+let placed,
+  placeRequest = false;
+let time = 0;
+
+// check for webxr session support
+if ("xr" in navigator) {
+  navigator.xr.isSessionSupported("immersive-ar").then((supported) => {
+    if (supported) {
+      //hide "ar-not-supported"
+      document.getElementById("ar-not-supported").style.display = "none";
+      init();
+    }
+  });
+}
+
+const init = async () => {
+  canvas = document.getElementById("renderCanvas");
+
+  engine = new BABYLON.Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true,
+    disableWebGL2Support: false,
+  });
+
+  engine.displayLoadingUI();
+
+  scene = new BABYLON.Scene(engine);
+  await createScene();
+
+  engine.runRenderLoop(function () {
+    if (scene && scene.activeCamera) {
+      scene.render();
+    }
+  });
+
+  // Resize
+  window.addEventListener("resize", () => {
+    if (!engine) return;
+    engine.resize();
+  });
+};
+
+const createScene = async () => {
+  const camera = new BABYLON.ArcRotateCamera(
+    "camera1",
+    2,
+    1,
+    5,
+    new BABYLON.Vector3(0.3, -0.5, 0),
+    scene
+  );
+
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
+
+  const hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData(
+    "/environmentSpecular.env",
+    scene
+  );
+  scene.environmentTexture = hdrTexture;
+
+  scene.environmenTexture = null;
+  // This creates a light
+  var light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(1, -1, 1),
+    scene
+  );
+
+  // Default intensity is 1. Let's dim the light a small amount
+  light.intensity = 0.5;
+
+  var xr = await scene.createDefaultXRExperienceAsync({
+    uiOptions: {
+      sessionMode: "immersive-ar",
+    },
+    optionalFeatures: true,
+  });
+
+  //remove VR laser pointers for AR
+  xr.pointerSelection.displayLaserPointer = false;
+  xr.pointerSelection.displaySelectionMesh = false;
+
+  const fm = xr.baseExperience.featuresManager;
+  const sm = xr.baseExperience.sessionManager;
+
+  // enable hit test
+  const xrTest = fm.enableFeature(BABYLON.WebXRHitTest, "latest");
+
+  // enable dom overlay
+  const domOverlayFeature = fm.enableFeature(
+    BABYLON.WebXRDomOverlay,
+    "latest",
+    { element: "#overlay" },
+    undefined,
+    false
+  );
+}
